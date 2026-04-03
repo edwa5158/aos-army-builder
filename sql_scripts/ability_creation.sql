e6f27e20a495de29495e12ee401fae69c9d9ff14
edwa5158 <jasonedwards2011@gmail.com>
2026-04-03 18:36:58 -0500

    fixed main.py and added scripts for ability creation

---
+++ sql_scripts/ability_creation.sql
@@ -0,0 +1,57 @@
+CREATE TABLE Ability (
+	[id] INTEGER PRIMARY KEY NOT NULL,
+	[name] TEXT NOT NULL,
+    [desc] TEXT NOT NULL,
+    [declare] TEXT,
+    [effect] TEXT
+);
+
+CREATE TABLE AbilityKeywords (
+	[ability_id] INTEGER NOT NULL,
+	[keyword_id] INTEGER NOT NULL,
+	FOREIGN KEY ("ability_id") REFERENCES "ability"("id") ON UPDATE CASCADE,
+	FOREIGN KEY ("keyword_id") REFERENCES "keyword"("id") ON UPDATE CASCADE,
+	PRIMARY KEY ("ability_id", "keyword_id")
+);
+
+CREATE TABLE Spell (
+	[id] INTEGER PRIMARY KEY NOT NULL,
+	[casting_value] INTEGER NOT NULL,
+	FOREIGN KEY ("id") REFERENCES "Ability"("id") ON UPDATE CASCADE
+);
+
+CREATE TABLE RoR (
+	[id] INTEGER PRIMARY KEY NOT NULL,
+	[name] TEXT NOT NULL,
+	[points] INTEGER NOT NULL
+	[desc] TEXT
+);
+
+CREATE TABLE Faction (
+	[id] INTEGER PRIMARY KEY NOT NULL
+	[name] TEXT NOT NULL
+);
+
+CREATE TABLE RoRIncl (
+	[ror_id] INTEGER NOT NULL,
+	[faction_id] INTEGER NOT NULL,
+	FOREIGN KEY ("ror_id") REFERENCES "RoR"("id") ON UPDATE CASCADE,
+	FOREIGN KEY ("faction_id") REFERENCES "Faction"("id") ON UPDATE CASCADE,
+	PRIMARY KEY("ror_id", "faction_id")
+);
+
+CREATE TABLE RoROrg (
+	[ror_id] INTEGER NOT NULL,
+	[warscroll_id] INTEGER NOT NULL,
+	[qty] INTEGER NOT NULL DEFAULT 1,
+	FOREIGN KEY ("ror_id") REFERENCES "RoR"("id") ON UPDATE CASCADE,
+	FOREIGN KEY ("warscroll_id") REFERENCES "Warscroll"("id") ON UPDATE CASCADE,
+	PRIMARY KEY ("ror_id", "warscroll_id")
+);
+
+CREATE TABLE RoRAbilities (
+	[ror_id] INTEGER NOT NULL,
+	[ability_id] INTEGER NOT NULL,
+	FOREIGN KEY ("ror_id") REFERENCES "RoR"("id") ON UPDATE CASCADE,
+	FOREIGN KEY ("ability_id") REFERENCES "Ability"("id") ON UPDATE CASCADE
+);
--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 from sqlalchemy.orm import Session
 
 from army.db import SessionLocal
-from army.models import WeaponAbility, WeaponProfile
+from army.models import WeaponProfile
 
 
 def main():
     session: Session = SessionLocal()
-    
-    rusty_weapons = session.query(WeaponProfile).where(WeaponProfile.name == "Rusty Weapons").one()
+
+    rusty_weapons = (
+        session.query(WeaponProfile).where(WeaponProfile.name == "Rusty Weapons").one()
+    )
 
     print(f"{rusty_weapons.name}")
     print(f"{rusty_weapons.attack}")
+
 
 if __name__ == "__main__":
     main()
